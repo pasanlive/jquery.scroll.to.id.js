c68c71baf25caba79982d4356652623d9231a6b3
Pasan Buddhika <pasanbuddhika@gmail.com>
2013-05-05 22:28:11 +0530

    initial commit

---
+++ jquery.scroll.to.id.js
@@ -0,0 +1,22 @@
+/**
+ *  Scroll to any element by ID
+ *
+ * by: Pasan Buddhika Weerathunga
+ *     me@pasanlive.com
+ * Date: 5/5/13
+ */
+
+(function ($) {
+    $.scrollToID = $.fn.scrollToID = function(id, gap){
+        gap = !isNaN(Number(gap))? gap : 50;
+        var x = $(id).offset().left + $(this).scrollLeft() - gap;
+        var y = $(id).offset().top + $(this).scrollTop() - gap;
+
+        if (!(this instanceof $)) return $.fn.scrollToID.apply($('html, body'), arguments);
+
+        return $(this).stop().animate({
+            scrollLeft: x,
+            scrollTop: y
+        });
+    };
+})(jQuery);
